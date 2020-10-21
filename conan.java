3f6f4c440d1a46f306efa59023284283733be6c1
Zaidan Zulhakim <59697378+Zaidannzzz@users.noreply.github.com>
2020-10-22 00:16:52 +0700

    Update conan.java

--- conan.java
+++ conan.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class conan {
     
     private String judul, nama;
-    private int tahun, harga;
+    private int data, tahun, harga;
     private double jual;
     	
     	//Constructor
-        conan(String judul, String nama, int tahun, int harga) {
+        conan(int data, String judul, String nama, int tahun, int harga) {
+            this.data = data;
             this.judul = judul;
             this.nama = nama;
             this.tahun = tahun;
@@ -16,38 +17,47 @@ public class conan {
         }
 
         public void display() {
+            System.out.println("Data Buku Ke  = " + this.data);
             System.out.println("Judul Buku    = " + this.judul);
             System.out.println("Nama Penerbit = " + this.nama);
             System.out.println("Tahun Terbit  = " + this.tahun);
             System.out.println("Harga Beli    = " + this.harga);
             jual = harga - (0.2 * harga);
             System.out.println("Harga Jual    = " + this.jual);
+            System.out.println("");
         }
 
  
 
     public static void main(String[] args) throws Exception {
-        String j;
-        String n;
-        int t;
-        int h;
-        
+        String j, n;
+        int d, t, h, q, o = 0;
 
-        Scanner userinput = new Scanner(System.in);
+        conan[] buku = new conan[3];
         System.out.println("Silahkan Input");
-        System.out.print("Judul Buku    = ");
-        j = userinput.next();
-        System.out.print("Nama Penerbit = ");
-        n = userinput.next();
-        Scanner userinput2 = new Scanner(System.in);
-        System.out.print("Tahun Terbit  = ");
-        t = userinput2.nextInt();
-        System.out.print("Harga         = ");
-        h = userinput2.nextInt();
-        System.out.println("");
-        conan buku = new conan(j, n, t, h);
-        buku.display();
-
+        while(o < 3){
+            Scanner userinput = new Scanner(System.in);
+            d = o + 1;
+            System.out.println("Data Buku Ke  = " + d);
+            System.out.print("Judul Buku    = ");
+            j = userinput.next();
+            System.out.print("Nama Penerbit = ");
+            n = userinput.next();
+            Scanner userinput2 = new Scanner(System.in);
+            System.out.print("Tahun Terbit  = ");
+            t = userinput2.nextInt();
+            System.out.print("Harga         = ");
+            h = userinput2.nextInt();
+            System.out.println("");
+            buku[o] = new conan(d, j, n, t, h);
+            o++;
+        }
+        System.out.println("===============");
+        System.out.println("=  Data Buku  =");
+        System.out.println("===============");
+        for(int z = 0; z < 3; z++) {
+			buku[z].display();
+		}
         System.exit(0);
     }
 }
